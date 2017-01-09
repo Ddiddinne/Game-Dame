13c998a4e717b5f7017f27934fa5267839a7c885
Sandrine Delecourt <sandredlcrt@hotmail.fr>
2017-01-09 14:28:49 +0100

    test to verify travis

---
+++ dame-core/src/test/java/org/isen/dame/core/FirstTests.java
@@ -0,0 +1,17 @@
+package org.isen.dame.core;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by Sandrine on 09/01/2017.
+ */
+public class FirstTests {
+
+    @Test
+    public void firstTest(){
+        assertThat(1).isEqualTo(1);
+    }
+
+}
