806c585119731a6883fafbd068195a3ef780d5a9
Sandrine Delecourt <sandredlcrt@hotmail.fr>
2017-01-16 16:17:20 +0100

    first jsp

---
+++ dame-webapp/src/main/java/org/isen/dame/webapp/ShowServlet.java
@@ -0,0 +1,21 @@
+package org.isen.dame.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Sandrine on 16/01/2017.
+ */
+@WebServlet("/totoAdress")
+public class ShowServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
---
+++ dame-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page import="org.isen.dame.webapp.ShowServlet" %>
+<html>
+<body>
+
+<h1>Play</h1>
+
+</body>
+</html>
