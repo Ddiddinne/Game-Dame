02072f6beb1bda3bc5a557da8126bcab8c84abc7
Sandrine Delecourt <sandredlcrt@hotmail.fr>
2017-01-26 13:14:28 +0100

    first resource (to clean)

---
+++ dame-webapp/src/main/java/org/isen/dame/webapp/model/Game.java
@@ -0,0 +1,51 @@
+package org.isen.dame.webapp.model;
+
+import org.isen.dame.core.Piece;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name="Game")
+public class Game {
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToMany(mappedBy="game", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @OrderColumn(name="index")
+    private List<Turn> turns = new ArrayList<>();
+
+    private String currentTurn = Piece.WHITE.toString();
+
+    public Game() {
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+
+    }
+
+    public List<Turn> getTurns() {
+        return turns;
+    }
+
+    public Piece getCurrentTurn() {
+        return  Piece.valueOf(currentTurn);
+    }
+
+    public void setCurrentTurn(Piece colour) {
+        currentTurn = colour.toString();
+    }
+
+}
---
+++ dame-webapp/src/main/java/org/isen/dame/webapp/service/PostResource.java
@@ -0,0 +1,44 @@
+package org.isen.dame.webapp.service;
+
+import org.isen.dame.webapp.dao.PostDAO;
+import org.isen.dame.webapp.model.Post;
+
+import javax.ws.rs.*;
+
+public class PostResource {
+
+
+    public PostResource() {
+
+    }
+
+    @Path("test")
+    @GET
+    public String doGet() {
+        return "Toto";
+    }
+
+    /*@PUT
+    public Post doUpdate(Post updatedPost) {
+        return dao.update(post.getId(), updatedPost.getUser(), updatedPost.getTitle(),
+                updatedPost.getContent());
+    }
+
+    @DELETE
+    public void doDelete() {
+        dao.delete(post.getId());
+    }
+
+    @Path("comments")
+    @POST
+    public Comment createComment(Comment newComment) {
+        return cdao.create(newComment.getAuthor(), newComment.getContent(), post.getId());
+    }
+    
+    @Path("comments")
+    @GET
+    public List<Comment> getComments() {
+        return cdao.list(post.getId());
+    }*/
+
+}
