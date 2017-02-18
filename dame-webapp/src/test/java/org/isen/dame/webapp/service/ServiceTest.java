872f76f0f404b0f91726b8c1074fa4a4b13a519a
marion59000 <marion.fioen@wanadoo.fr>
2017-02-18 16:48:54 +0100

    problem get list chip

--- dame-webapp/src/test/java/org/isen/dame/webapp/service/ServiceTest.java
+++ dame-webapp/src/test/java/org/isen/dame/webapp/service/ServiceTest.java
@@ -1,5 +1,6 @@
 package org.isen.dame.webapp.service;
 
+import org.isen.dame.core.Chip;
 import org.isen.dame.core.DameGame;
 import org.isen.dame.core.DameGameImpl;
 import org.isen.dame.core.Game;
@@ -28,7 +29,6 @@ public class ServiceTest {
 
     private Game game;
     private List<Game> list;
-
     @Before
     public void setUp(){
         game = new Game();
@@ -44,5 +44,38 @@ public class ServiceTest {
         List<Game> test = service.testGame();
         assertEquals(test, list);
 
+    }
+
+    @Test
+    public void testChips(){
+        List<Chip> test = service.testChip();
+        assertEquals(test, daoChip.getChips("123456789"));
+    }
+
+    @Test
+    public void testNewGame(){
+        //begin game
+        Game games=service.createNewGame();
+        //test player init
+        assertEquals(games.getCurrentTurn().toString(),"WHITE");
+        //test positionPiece
+        String token=games.getToken();
+        List<Chip> test=service.getChips(token);
+        for(Chip chip:test){
+            System.out.println(chip.getPosition());
+        }
+    }
+
+    @Test
+    public void testPlay(){
+        //test move piece in an empty case
+        //test move piece in not an empty case
+        //test move piece when eat another piece from other color
+        //test move piece when eat another piece from same color
+    }
+
+    @Test
+    public void testTurn(){
+        //test getTurn and changeTurn
     }
 }
