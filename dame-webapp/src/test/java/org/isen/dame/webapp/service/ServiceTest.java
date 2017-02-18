4894ccf956d44f192feee415bcf29ccd9730cd98
marion59000 <marion.fioen@wanadoo.fr>
2017-02-18 17:47:11 +0100

    add test chips

--- dame-webapp/src/test/java/org/isen/dame/webapp/service/ServiceTest.java
+++ dame-webapp/src/test/java/org/isen/dame/webapp/service/ServiceTest.java
@@ -1,9 +1,6 @@
 package org.isen.dame.webapp.service;
 
-import org.isen.dame.core.Chip;
-import org.isen.dame.core.DameGame;
-import org.isen.dame.core.DameGameImpl;
-import org.isen.dame.core.Game;
+import org.isen.dame.core.*;
 import org.isen.dame.webapp.DAO.DaoChip;
 import org.isen.dame.webapp.DAO.DaoGame;
 import org.junit.Before;
@@ -32,19 +29,12 @@ public class ServiceTest {
     private List<Game> list;
     private Game game2;
     private List<Chip> chip;
-    //@Before
-    /*public void setUp(){
-        game = new Game();
-        game.setToken("123456789");
-        list = new ArrayList<>();
-        list.add(game);
-        when(daoGame.test()).thenReturn(list);
-    }*/
 
     @Before
     public void setUp(){
         game = new Game();
         game.setToken("123456789");
+        game.setCurrentTurn(Piece.WHITE);
         list = new ArrayList<>();
         list.add(game);
 
@@ -56,17 +46,14 @@ public class ServiceTest {
             Chip chipValue=new Chip("123456789",entry.getKey(),entry.getValue());
             chip.add(chipValue);
         }
-
         when(daoGame.test()).thenReturn(list);
         when(daoChip.test()).thenReturn(chip);
     }
 
     @Test
     public void testGame(){
-
         List<Game> test = service.testGame();
         assertEquals(test, list);
-
     }
 
     @Test
@@ -75,30 +62,4 @@ public class ServiceTest {
         assertEquals(test, chip);
     }
 
-    @Test
-    public void testNewGame(){
-        //begin game
-        Game games=service.createNewGame();
-        //test player init
-        assertEquals(games.getCurrentTurn().toString(),"WHITE");
-        //test positionPiece
-        String token=games.getToken();
-        List<Chip> test=service.getChips(token);
-        for(Chip chip:test){
-            System.out.println(chip.getPosition());
-        }
-    }
-
-    @Test
-    public void testPlay(){
-        //test move piece in an empty case
-        //test move piece in not an empty case
-        //test move piece when eat another piece from other color
-        //test move piece when eat another piece from same color
-    }
-
-    @Test
-    public void testTurn(){
-        //test getTurn and changeTurn
-    }
 }
